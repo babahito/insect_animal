66edd591bbf23e1a2c9364541a40bb37b599ac8e
babahito <babahito@gmail.com>
2020-11-20 11:17:15 +0900

    ログインおよび新規会員登録の作成

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,16 @@
+
+
+
+import './bootstrap'
 import Vue from 'vue'
-// ルーティングの定義をインポートする
 import router from './router'
-// Storeを定義する
 import store from './store'
-// ルートコンポーネントをインポートする
 import App from './App.vue'
 
 new Vue({
   el: '#app',
-  router, // ルーティングの定義を読み込む
-  components: { App }, // ルートコンポーネントの使用を宣言する
-  template: '<App />' // ルートコンポーネントを描画する
+  router,
+  store,
+  components: { App },
+  template: '<App />'
 })
